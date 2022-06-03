7866990a9ea80b88d0fb96c94b7d8244f655d5c6
Snowy7 <snowydev7@gmail.com>
2022-06-03 17:41:42 +0300

    Deploying first

---
+++ index.js
@@ -0,0 +1,40 @@
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client();
+console.log('Connecting...');
+
+let welcomeMsg = `╭───── • 𒀭 • ─────╮
+◘ يُرجى منك تعبئة الاستمارة◘
+
+●اختر اللقب :
+「」
+●اللقب من انمي :
+「」
+● اختر نقابه :
+「𝐒𝐢𝐥𝐯𝐢𝐚 ✤」
+「𝐇𝐮𝐥𝐢𝐚 𖤓」
+
+● ذكر ام انثى :
+「」
+
+● كيف حصلت على الروابط :
+「」
+╰───── • 𒀭 • ─────╯
+𝄪𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐄𝐥𝐥𝐢𝐞𝄪`
+
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED');
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+client.on('message', message => {
+	if(message.body === 'استمارة') {
+		message.reply(welcomeMsg);
+	}
+});
+console.log('Initializing...');
+client.initialize();
